024c91226b939d8cf5bdd29e425acab8691b913f
jennyj7 <47221554+jennyj7@users.noreply.github.com>
2021-04-02 20:30:53 -0400

    Add files via upload

--- get_distance_server.py
+++ get_distance_server.py
@@ -1,13 +1,25 @@
+#Code from: https://stackoverflow.com/questions/60486029/how-to-find-the-center-of-black-objects-in-an-image-with-python-opencv
+#Code from: https://stackoverflow.com/questions/29447333/python-taking-a-picture-with-raspberry-pi-camera
+        
+
+#import libries
 import socket
+import time
+import picamera
+import numpy as np
+import cv2 as cv2
 
 #Set host and port
 HOST = '127.0.0.1'
-PORT = 9997
+PORT = 9999
 
 #Create Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST,PORT))
 s.listen(5)
+
+#initialize camera
+cam = picamera.PiCamera()
 
 #update command line to show it is waiting for a connection
 print("[Server 01] Waiting for a connection")
@@ -27,60 +39,51 @@ while (1):
     #check what the command is
     if command.decode() == "start":
 
-        #Code from: https://stackoverflow.com/questions/29447333/python-taking-a-picture-with-raspberry-pi-camera
         #Tells camera to take picture and save it as a jpg
+        while True:
 
-        #import libries
-        import time
-        import picamera
+            #name jpg
+            readImage = 'centeredDot.jpg'
 
-        #name jpg
-        readImage = 'blackDot.jpg'
+            #tell pi to take a picture with the camera
+            #cam.capture(readImage)
 
-        #tell pi to take a picture with the camera
-        #cam = picamera.PiCamera()
-        #cam.capture(readImage)
+            #update command line that picture has been taken
+            print('[Server 04] Picture Taken')
 
-        #update command line that picture has been taken
-        print('[Server 04] Picture Taken')
+            #Add image
+            image = cv2.imread(readImage)
 
-        #Code from: https://stackoverflow.com/questions/60486029/how-to-find-the-center-of-black-objects-in-an-image-with-python-opencv
-        #Reads jpg and finds circles in the image
+            #Make a copy of the image
+            original = image.copy()
 
-        #import the necessary packages
-        import numpy as np
-pip
+            #Read in image in grey and blur the image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (3,3), 0)
 
-        #matplotlib shows the image
-        from matplotlib import pyplot as plt
+            #Set the threshold values to find black
+            thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        #Add image
-        image = cv2.imread(readImage)
+            #Find contours
+            ROI_number = 0
+            cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-        #Make a copy of the image
-        original = image.copy()
-
-        #Read in image in grey and blur the image
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (3,3), 0)
-
-        #Set the threshold values to find black
-        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-        #Find contours
-        ROI_number = 0
-        cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-        for c in cnts:
             #Obtain bounding rectangle to get measurements
-            x,y,w,h, = cv2.boundingRect(c)
+            x,y,w,h, = cv2.boundingRect(cnts[0])
             
             #Check for centroid
-            M = cv2.moments(c)
+            M = cv2.moments(cnts[0])
             
             #counter for number of retakes
             retakeCount = 0
+            
+            #update command line if no circle is found
+            if(M["m00"]) == 0:
+                print("[Server 00] No Dot Found. Reposition Arm.")
+                contents = "Realign"
+                connection.send(contents.encode())
+                break
             
             #If circle is not found take a new picture
             while int(M["m00"]) == 0:
@@ -113,42 +116,77 @@ pip
                 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
                 
                 #Obtain bounding rectangle to get measurements
-                x,y,w,h, = cv2.boundingRect(c)
+                x,y,w,h, = cv2.boundingRect(cnts[0])
                 
                 #Check for centerioid
-                M = cv2.moments(c)
-                
-                #Show the image in a new window
-                plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
-                plt.xticks([]), plt.yticks([])
-                plt.show()
-                
-                
+                M = cv2.moments(cnts[0])                    
+                    
             #update command line
             print("[Server 07] Dot Found!")
             
             #Find centroid
             cX = int(M["m10"]/M["m00"])
             cY = int(M["m01"]/M["m00"])
-            
+
             #Crop and save ROI
             ROI = original[y:y+h, x:x+w]
             cv2.imwrite('ROI_{}.png'.format(ROI_number),ROI)
             ROI_number += 1
-            
+
             #Draw the contour and center of the shape on the image
             cv2.rectangle(image,(x,y),(x+w,y+h),(36,255,12),4)
             cv2.circle(image,(cX,cY),10,(320,159,22),-1)
+            
+            #Find the center of the whole jpg
+            imageTuple = image.shape
+            centerX = int(imageTuple[1]/2)
+            centerY = int(imageTuple[0]/2)
+            
+            #Draw center of circle
+            cv2.circle(image,(centerX,centerY),10,(110,40,100),-1)
+            #draw hypotenuse
+            cv2.line(image, (cX,cY), (centerX,centerY), (57, 255, 20), 1)
+            #draw x line
+            cv2.line(image, (cX,cY), (centerX,cY), (57, 255, 20), 1)
+            #draw y line
+            cv2.line(image, (centerX,centerY), (centerX,cY), (57, 255, 20), 1)
 
-            #Show the image in a new window
-            plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
-            plt.xticks([]), plt.yticks([])
+            #update marked picture
+            cv2.imwrite('image.png', image)
             
-            #Send data back to first raspberry pi
-            contents = "123 567"
-            connection.send(contents.encode())       
+            #show marked image
+            markedImage = cv2.imread('image.png')
+            cv2.imshow('image',markedImage)
             
-            #update command line
-            print("[Server 08] Sent Information Back to Client")
-            print("-------------------------------------------")
-            print("[Server 09] Waiting for a new connection")   
+            #ask for keyboard command
+            k = cv2.waitKey(0)
+            print("Is this the correct dot? [y/n]")
+            
+            if k == ord('y'):
+                xAlign = centerX - cX
+                yAlign = cY - centerY
+                cv2.destroyAllWindows()
+                    
+                #Send information back to client
+                if(xAlign <= 10 and xAlign >= -10 and yAlign <= 10 and yAlign >= -10):
+                    contents = "Aligned"
+                    connection.send(contents.encode())
+                    break
+                
+                #Send data back to first raspberry pi
+                else:
+                    contents = str(xAlign) + " " + str(yAlign)
+                    connection.send(contents.encode())   
+                    break
+                                
+            elif k == ord('e'):
+                cv2.destroyAllWindows()
+                check = True
+            else:
+                cv2.destroyAllWindows()
+            
+        
+        #update command line
+        print("[Server 08] Sent Information Back to Client")
+        print("-------------------------------------------")
+        print("[Server 09] Waiting for a new connection")   
